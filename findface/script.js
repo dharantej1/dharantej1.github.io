cf1061558f837f36a968ed5947418406e02daf69
dharantej1 <pusthakala.dharan@gmail.com>
2022-12-29 23:09:36 +0530

    bootstrap update

--- findface/script.js
+++ findface/script.js
@@ -1,37 +1,12 @@
-function findFriend() {
-    // Get the roll number from the input field
-    var rollNumber = document.getElementById("roll-number").value.toUpperCase();
-
-    // Update the src attribute of the image element to display the friend's image
-    document.getElementById("friend-image").src = "http://teleuniv.in/sanjaya/student-images/" + rollNumber + ".jpg";
-
-    // Show the right column
-    document.querySelector(".right-column").style.display = "block";
-
-    // Show the download button
-    document.getElementById("download-button").style.display = "block";
-}
-
-function downloadImage() {
-    // Get the image element
-    var imageElement = document.getElementById("friend-image");
-
-    // Create a link element
-    var linkElement = document.createElement("a");
-
-    // Set the href attribute of the link to the src attribute of the image
-    linkElement.href = imageElement.src;
-
-    // Set the download attribute of the link to the file name
-    linkElement.download = "friend.jpg";
-
-    //
-    // Add the link to the DOM
-    document.body.appendChild(linkElement);
-
-    // Click the link to initiate the download
-    linkElement.click();
-
-    // Remove the link from the DOM
-    document.body.removeChild(linkElement);
-}
+$(document).ready(function() {
+    $('#search-form').submit(function(e) {
+      e.preventDefault();
+      var rollNumber = $('#roll-number').val().toUpperCase();
+      if (rollNumber) {
+        var imageUrl = 'http://teleuniv.in/sanjaya/student-images/' + rollNumber + '.jpg';
+        $('#image').attr('src', imageUrl).removeClass('d-none');
+        $('#download-link').attr('href', imageUrl).removeClass('d-none');
+      }
+    });
+  });
+  
