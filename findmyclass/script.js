dd6d24e7d6369650c1843f9e10897956733e49d4
dharantej1 <pusthakala.dharan@gmail.com>
2023-02-07 17:28:54 +0530

    version 2

--- findmyclass/script.js
+++ findmyclass/script.js
@@ -1,7 +1,3 @@
-document.querySelectorAll('.tile').forEach(tile => {
-    tile.addEventListener('click', function() {
-      this.classList.toggle('clicked');
-      this.nextElementSibling.style.marginLeft = this.classList.contains('clicked') ? '280px' : '20px';
-    });
-  });
-  
+$(document).ready(function() {
+  $("#card-carousel").carousel();
+});
