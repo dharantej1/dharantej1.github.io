da4e5d3984b4835c995bff1f5ecf540c0bc9f508
dharantej1 <pusthakala.dharan@gmail.com>
2023-02-07 17:50:56 +0530

    version 3

--- findmyclass/script.js
+++ findmyclass/script.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
-  $("#card-carousel").carousel();
+  $('.card-3d').click(function() {
+    var image = $(this).find('img').attr('src');
+    var modal = `
+      <div class="modal fade" id="imageModal" tabindex="-1" role="dialog" aria-labelledby="imageModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+          <div class="modal-content">
+            <div class="modal-body">
+              <img src="${image}" alt="Modal Image">
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+
+    $('body').append(modal);
+    $('#imageModal').modal();
+  });
 });
